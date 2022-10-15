96d8ca264591681673eeece2192dbc7ab58815f4
Okiemute Gold <goldokiemute@gmail.com>
2022-10-15 15:41:17 +0100

    gitinored setup data

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import config from "../../firebaseConfig.txt";
 
 var firebaseConfig = {
-  apiKey: config[0].apiKey,
-  authDomain: config[0].authDomain,
-  projectId: config[0].projectId,
-  storageBucket: config[0].storageBucket,
-  messagingSenderId: config[0].messagingSenderId,
-  appId: config[0].appId,
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGE_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
